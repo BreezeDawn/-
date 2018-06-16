7dd68e7601c68cc4b7568814705fa3167f039fe4
breezedawn <40269040+breezedawn@users.noreply.github.com>
2018-06-16 10:50:34 +0800

    Update and rename 习题6.py to 习题7.py

---
+++ Base/Day05/Exercises/习题7.py
@@ -0,0 +1,12 @@
+# 写一个程序，由用户输入一个年份，调用函数判断是否是闰年。
+#     * 年份作为参数传递给函数
+#     * 函数需要有返回值
+
+
+# year = int(input('请输入年份：'))
+# def judge(a):
+#     if a % 4 == 0:
+#         return '闰年'
+#     else:
+#         return '平年'
+# print(judge(year))
