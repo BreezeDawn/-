2537decb7688defcde14533391097a792f951a70
breezedawn <40269040+breezedawn@users.noreply.github.com>
2018-06-16 10:56:23 +0800

    Create 习题9.py

---
+++ Base/Day05/Exercises/习题9.py
@@ -0,0 +1,12 @@
+# 斐波那契数列
+
+# num1 = 0
+# num2 = 1
+# count = 0
+# n = int(input('输入n：'))
+# while count < n:
+#     num3 = num2 +num1
+#     num1 = num2
+#     num2 = num3
+#     count += 1
+#     print(num1,end=' ')
