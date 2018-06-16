704759b2ab7410d7c26d1b3ffb384ab9da323bd3
breezedawn <40269040+breezedawn@users.noreply.github.com>
2018-06-16 10:54:35 +0800

    Update 习题6.py

--- Base/Day05/Exercises/习题6.py
+++ Base/Day05/Exercises/习题6.py
@@ -1,10 +1,10 @@
 # 写一个简单的函数嵌套：调用test1，在test1调用test2，
 # 所有函数的开始和结束位置都打印一行文本，验证调用执行顺序
 
-# def test2():
-#     print('222222')
-# def test1():
-#     print('111111')
-#     test2()
-#     print('111111')
-# test1()
+def test2():
+    print('222222')
+def test1():
+    print('111111')
+    test2()
+    print('111111')
+test1()
