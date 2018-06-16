18ec0a0c9bedfe496e42be2640ee50fbb99777e7
breezedawn <40269040+breezedawn@users.noreply.github.com>
2018-06-16 10:55:30 +0800

    Update 习题5.py

--- Base/Day05/Exercises/习题5.py
+++ Base/Day05/Exercises/习题5.py
@@ -1,16 +1,16 @@
 # 将习题4计算结果作为返回值,调用函数并打印调用结果。
 
-# def add_add(a,b):
-#     if type(eval(b)) is float:
-#         b = int(eval(b))
-#     else:
-#         b = int(b) - 1
-#     res = 0
-#     while a <= b:
-#         res = res + a
-#         a += 1
-#     return res
-# num1 = int(eval(input('输入第一个数:')))+ 1
-# num2 = input('输入第二个数:')
-# res = add_add(num1,num2)
-# print(res)
+def add_add(a,b):
+    if type(eval(b)) is float:
+        b = int(eval(b))
+    else:
+        b = int(b) - 1
+    res = 0
+    while a <= b:
+        res = res + a
+        a += 1
+    return res
+num1 = int(eval(input('输入第一个数:')))+ 1
+num2 = input('输入第二个数:')
+res = add_add(num1,num2)
+print(res)
